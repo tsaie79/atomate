bd21550e93e7e917c50845cf7bc2884824b200a5
jeng-yuantsai <tsaie79@gmail.com>
2020-09-09 15:28:14 -0400

    update

--- atomate/vasp/workflows/jcustom/wf_full.py
+++ atomate/vasp/workflows/jcustom/wf_full.py
@@ -207,10 +207,6 @@ def get_wf_full_scan(structure, charge_states, gamma_only, dos_hse, nupdowns, en
         # FW2 Run SCAN SCF
         uis_scan_scf = {
             "user_incar_settings": {
-                # "AMIX": 0.2,
-                # "AMIX_MAG": 0.8,
-                # "BMIX": 0.0001,
-                # "BMIX_MAG": 0.0001,
                 "EDIFF": 1e-05,
                 "ENCUT": encut,
                 "ISMEAR": 0,
