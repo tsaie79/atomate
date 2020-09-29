901f85071d79710cce80baac160f86ff55b909a8
jeng-yuantsai <tsaie79@gmail.com>
2020-09-28 20:53:43 -0400

    force gamma = True

--- atomate/vasp/workflows/jcustom/gw_workflow.py
+++ atomate/vasp/workflows/jcustom/gw_workflow.py
@@ -14,7 +14,8 @@ import numpy as np
 def gw_wf(structure, ncores, vis_static=None, vasp_input_set_params=None, vasptodb=None, wf_addition_name=None):
     fws = []
     # 1. STATIC
-    static_fw = StaticFW(structure, vasp_input_set=vis_static, name="gw_static")
+    static_fw = StaticFW(structure, vasp_input_set=vis_static, vasp_input_set_params={"force_gamma": True},
+                         name="gw_static")
 
     # 2. DIAG
     diag_fw = JMVLGWFW(structure, ncores=ncores, parents=static_fw, vasp_input_set_params=vasp_input_set_params,
