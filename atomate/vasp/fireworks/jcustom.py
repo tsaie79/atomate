ea5aeca6b2d439cc50f654168a1047085be046c3
jeng-yuantsai <tsaie79@gmail.com>
2021-01-15 23:05:56 -0500

    update

--- atomate/vasp/fireworks/jcustom.py
+++ atomate/vasp/fireworks/jcustom.py
@@ -531,7 +531,7 @@ class JHSEStaticFW(Firework):
         
         t.append(RunVaspCustodian(vasp_cmd=vasp_cmd, auto_npar=">>auto_npar<<"))
         t.append(PassCalcLocs(name=name))
-        t.append(VaspToDb(db_file=db_file, **vasptodb_kwargs))
+        # t.append(VaspToDb(db_file=db_file, **vasptodb_kwargs))
         super(JHSEStaticFW, self).__init__(t, parents=parents, name=fw_name, **kwargs)
 
 class JHSESOCFW(Firework):
