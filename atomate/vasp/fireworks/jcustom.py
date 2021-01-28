f2dbb7ebda9e1279ac26dd2659856d782264cb0d
jeng-yuantsai <tsaie79@gmail.com>
2021-01-28 12:30:23 -0500

    update

--- atomate/vasp/fireworks/jcustom.py
+++ atomate/vasp/fireworks/jcustom.py
@@ -485,12 +485,13 @@ class JHSEStaticFW(Firework):
 
         fw_name = "{}-{}".format(structure.composition.reduced_formula if structure else "unknown", name)
 
-        if prev_calc_dir:
-            if not parents:
-                t.append(CopyVaspOutputs(calc_dir=prev_calc_dir, contcar_to_poscar=True, additional_files=["WAVECAR"]))
-            else:
-                # used in soc when CHGCAR from nosocSCF is neeeded
-                t.append(CopyFiles(from_dir=prev_calc_dir, files_to_copy=["CHGCAR.gz", "CHGCAR"], continue_on_missing=True))
+
+        if prev_calc_dir and parents:
+            t.append(CopyVaspOutputs(calc_loc=prev_calc_loc, contcar_to_poscar=True))
+            t.append(CopyFiles(from_dir=prev_calc_dir, files_to_copy=["CHGCAR.gz", "CHGCAR"], continue_on_missing=True))
+            t.append(WriteVaspHSEBSFromPrev(mode="uniform", reciprocal_density=None, kpoints_line_density=None))
+        elif prev_calc_dir:
+            t.append(CopyVaspOutputs(calc_dir=prev_calc_dir, contcar_to_poscar=True, additional_files=["WAVECAR"]))
             t.append(WriteVaspHSEBSFromPrev(mode="uniform", reciprocal_density=None, kpoints_line_density=None))
         elif parents:
             if prev_calc_loc:
